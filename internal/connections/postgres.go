73d05d4bd219208773ef9f3a2e23e88877051869
Ekaterina <katyachkalovski@gmail.com>
2025-07-23 19:06:11 +0500

    formatting

--- internal/connections/postgres.go
+++ internal/connections/postgres.go
@@ -3,12 +3,13 @@ package connections
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"os"
+
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/jackc/pgx/v5/stdlib"
-	"log"
-	"os"
 )
 
 var PGDatabase *sql.DB
